465042452ef022a401664b2a9b9dc3998eb836bc
EvoX7 <creativesoultouch@gmail.com>
2022-07-06 17:41:55 +0200

    Milestone 3

--- js/main.js
+++ js/main.js
@@ -3,41 +3,45 @@ const app = new Vue({
   data: {
     todos: [
       {
-        text: 'Study code',
+        text: "Study code",
         done: false
       },
       {
-        text: 'Drink water to not evaporate',
+        text: "Drink water to not evaporate",
         done: true
       },
       {
-        text: 'Complete assigned tasks',
+        text: "Complete assigned tasks",
         done: true
       },
       {
-        text: 'Take five minutes to yourself',
+        text: "Take five minutes to yourself",
         done: false
       },
 
       {
-        text: 'Do the laundry',
+        text: "Do the laundry",
         done: false
-      },
+      }
     ],
 
-    newTodo: '',
-
+    newTodo: ""
   },
 
+  // ? Delete task
   methods: {
     deleteFromList(item) {
       this.todos.splice(item, 1);
     },
 
+    // * Add task
     addToList: function (newTask) {
-        this.todos.push(newTask);
-        this.newTodo = '';
+      if (newTask == "") {
+        alert("Please, specify a new task!");
+      } else {
+        this.todos.push({ text: newTask, done: false });
+        this.newTodo = "";
+      }
+    }
   }
-}
-
 });
